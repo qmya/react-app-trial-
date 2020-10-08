226666b08f527694018f31d23e4c3edea828353c
qmya <qandeelgul90@gmail.com>
2020-10-07 22:29:46 -0400

    added the material UI button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import logo from './assets/pocket.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/pocket.svg";
+import Button from "@material-ui/core/Button";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    
-        <img src={logo} className="App-logo" alt="logo" />
-        <button> Qandeel Mya </button>
+      <img src={logo} className="App-logo" alt="logo" />
+      <Button variant="contained" color="primary">
+        Qandeel Mya
+      </Button>
     </div>
   );
 }
